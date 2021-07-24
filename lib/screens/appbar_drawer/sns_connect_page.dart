0d7095df6d8c22eec77b633afaac713c25bdc113
ohyeaho <ohyeah7275@gmail.com>
2021-07-25 04:51:24 +0900

    sns connect page 追加

---
+++ lib/screens/appbar_drawer/sns_connect_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:toridori_clone/components/back_appbar.dart';
+
+class SnsConnectPage extends StatelessWidget {
+  const SnsConnectPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: BackAppbar(title: 'SNSを連携する'),
+    );
+  }
+}
