3e7511aafe6c50487b99225e7c28876c0e0b8169
ohyeaho <ohyeah7275@gmail.com>
2021-07-29 11:46:38 +0900

    favorite page UI

--- lib/screens/favorite_page.dart
+++ lib/screens/favorite_page.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:toridori_clone/components/main_page_appbar.dart';
+import 'package:toridori_clone/components/main_page_drawer.dart';
 
 class FavoritePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text('お気に入り');
+    return Scaffold(
+      appBar: MainPageAppbar(
+        title: Text(
+          'お気に入り',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+      ),
+      drawer: SafeArea(child: MainPageDrawer()),
+    );
   }
 }
