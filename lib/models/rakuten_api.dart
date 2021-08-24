f57f183d4eefa35a03203ba62045ef8fa1df6750
ohyeah7275@gmail.com <ohyeah7275@gmail.com>
2021-08-24 17:06:48 +0900

    get recommended items on home page from rakuten api

--- lib/models/rakuten_api.dart
+++ lib/models/rakuten_api.dart
@@ -3,65 +3,95 @@ import 'dart:convert';
 import 'package:http/http.dart';
 
 class Item {
-  String name;
-  String image;
-  String title;
-  String caption;
-  int? price;
+  String name; // 商品の名前
+  String image; // 商品画像
+  String caption; // 商品の説明
+  int? price; // 商品の値段
   DateTime? time;
 
   Item({
     this.name = '',
     this.image = '',
-    this.title = '',
     this.caption = '',
     this.price,
     this.time,
   });
 
-  // static Future<Map<String, List<Item>>?> getItem(String keyword) async {
-  //   var _keyword = Uri.encodeComponent(keyword);
-  //   Map<String, List<Item>>? response = {};
-  //   String url =
-  //       'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?keyword=$_keyword&format=json&applicationId=1018749470638496278';
-  //   try {
-  //     var result = await get(Uri.parse(url));
-  //     Map<String, dynamic> data = jsonDecode(result.body);
-  //     List<dynamic> itemData = data['Items'];
-  //     List<Item> items = itemData.map((item) {
-  //       return Item(
-  //         name: item['Item']['itemName'],
-  //       );
-  //     }).toList();
-  //     response['Items'] = items;
-  //     return response;
-  //   } catch (e) {
-  //     print('エラー: $e');
-  //     return null;
-  //   }
-  // }
-
-  static Future<Map<String, String>?> getItem(String keyword) async {
-    var _keyword = Uri.encodeComponent(keyword);
+  // item info test
+  static Future<Item?> getItem(String keyword) async {
+    var _keyword = Uri.encodeComponent(keyword); // utf-8にエンコード
     var applicationId = 1018749470638496278;
+    // print(_keyword);
     String url =
         'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?&format=json&applicationId=$applicationId&keyword=$_keyword';
+    // var result = await get(Uri.parse(url));
+    // Map<String, dynamic> data = jsonDecode(result.body);
+    // Item items = Item(
+    //   name: data['Items'][0]['Item']['itemName'],
+    //   image: data['Items'][0]['Item']['mediumImageUrls'][0]['imageUrl'],
+    //   price: data['Items'][0]['Item']['itemPrice'],
+    // );
+    // return items;
     try {
       var result = await get(Uri.parse(url));
       Map<String, dynamic> data = jsonDecode(result.body);
-      Map<String, String>? response = {};
-      if (data['Items'] != null) {
-        response['Items'] = data['Items'][0]['Item']['itemName'];
-      } else {
-        response['message'] = '正しい検索キーワードを指定してください';
-      }
-      return response;
+      Item items = Item(
+        name: data['Items'][0]['Item']['itemName'],
+        image: data['Items'][0]['Item']['mediumImageUrls'][0]['imageUrl'],
+        price: data['Items'][0]['Item']['itemPrice'],
+      );
+      return items;
     } catch (e) {
       print('エラー: $e');
       return null;
     }
   }
 }
+
+// get api test
+//   static Future<Map<String, String>?> getItem(String keyword) async {
+//     var _keyword = Uri.encodeComponent(keyword); // utf-8にエンコード
+//     var applicationId = 1018749470638496278;
+//     String url =
+//         'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?&format=json&applicationId=$applicationId&keyword=$_keyword';
+//     try {
+//       var result = await get(Uri.parse(url));
+//       Map<String, dynamic> data = jsonDecode(result.body);
+//       Map<String, String>? response = {};
+//       if (data['Items'] != null) {
+//         response['Items'] = data['Items'][0]['Item']['itemName'];
+//       } else {
+//         response['message'] = '正しい検索キーワードを指定してください';
+//       }
+//       return response;
+//     } catch (e) {
+//       print('エラー: $e');
+//       return null;
+//     }
+//   }
+// }
+
+// static Future<Map<String, List<Item>>?> getItem(String keyword) async {
+//   var _keyword = Uri.encodeComponent(keyword);
+//   Map<String, List<Item>>? response = {};
+//   String url =
+//       'https://app.rakuten.co.jp/services/api/IchibaItem/Search/20170706?keyword=$_keyword&format=json&applicationId=1018749470638496278';
+//   try {
+//     var result = await get(Uri.parse(url));
+//     Map<String, dynamic> data = jsonDecode(result.body);
+//     List<dynamic> itemData = data['Items'];
+//     List<Item> items = itemData.map((item) {
+//       return Item(
+//         name: item['Item']['itemName'],
+//       );
+//     }).toList();
+//     response['Items'] = items;
+//     return response;
+//   } catch (e) {
+//     print('エラー: $e');
+//     return null;
+//   }
+// }
 
 // class ItemService{
 //   static const _uri =
--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:toridori_clone/components/main_page_appbar.dart';
 import 'package:toridori_clone/components/main_page_drawer.dart';
@@ -23,21 +24,23 @@ class _HomePageState extends State<HomePage> {
     Category(name: 'その他', image: 'images/home_top_other.jpg'),
   ];
   List<String> sort = ['すべて', '報酬がもらえる', '同伴者も無料', '自宅でできる'];
-  List<String> item = [
-    'https://www.tokyotravelpartners.jp/img/slide/sd04s.jpg',
-    'https://img-cdn.guide.travel.co.jp/article/485/32832/EF34EAEE0DC54909A5D6031948F66BAC_L.jpg',
-    'https://www.tokyotravelpartners.jp/img/slide/sd04s.jpg',
-    'https://img-cdn.guide.travel.co.jp/article/485/32832/EF34EAEE0DC54909A5D6031948F66BAC_L.jpg',
-    'https://www.tokyotravelpartners.jp/img/slide/sd04s.jpg',
-    'https://img-cdn.guide.travel.co.jp/article/485/32832/EF34EAEE0DC54909A5D6031948F66BAC_L.jpg',
-    'https://www.tokyotravelpartners.jp/img/slide/sd04s.jpg',
-    'https://img-cdn.guide.travel.co.jp/article/485/32832/EF34EAEE0DC54909A5D6031948F66BAC_L.jpg',
-    'https://www.tokyotravelpartners.jp/img/slide/sd04s.jpg',
-    'https://img-cdn.guide.travel.co.jp/article/485/32832/EF34EAEE0DC54909A5D6031948F66BAC_L.jpg',
-    'https://www.tokyotravelpartners.jp/img/slide/sd04s.jpg',
-  ];
-  String? itemName;
+  Item? item;
   String? errorMessage;
+
+  static const spinKit = SpinKitCircle(
+    color: Colors.red,
+    size: 100.0,
+  );
+
+  // rakuten api 取得
+  @override
+  void initState() {
+    super.initState();
+    Future(() async {
+      item = await Item.getItem('おすすめ');
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -99,31 +102,33 @@ class _HomePageState extends State<HomePage> {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              TextField(
-                onSubmitted: (value) async {
-                  // Map<String, String>? response = {};
-                  // response =
-                  Map<String, String>? response = {};
-                  response = await Item.getItem(value);
-                  errorMessage = response!['message'];
-                  if (response.containsKey('Items')) {
-                    itemName = response['Items'];
-                  }
-                  print('test: $itemName');
-                  setState(() {});
-                },
-              ),
-              Text(itemName != null ? itemName! : ''),
-              Text(
-                errorMessage != null ? errorMessage! : '',
-                style: TextStyle(color: Colors.red),
-              ),
+              // TextField(
+              //   onSubmitted: (value) async {
+              //     item = await Item.getItem('おすすめ');
+              //     // Map<String, String>? response = {};
+              //     // response = await Item.getItem(value);
+              //     // errorMessage = response!['message'];
+              //     // if (response.containsKey('Items')) {
+              //     //   itemName = response['Items'];
+              //     // }
+              //     // print('test: $itemName');
+              //     setState(() {});
+              //   },
+              // ),
+
+              /// rakuten api test
+              // Text(itemName != null ? itemName! : ''),
+              // Text(
+              //   errorMessage != null ? errorMessage! : '',
+              //   style: TextStyle(color: Colors.red),
+              // ),
               // itemName != ''
               //     ? Text(
               //         itemName!,
               //         style: TextStyle(color: Colors.white),
               //       )
               //     : Container(),
+              ///ここまでtest
               SizedBox(height: 20),
               SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
@@ -160,35 +165,27 @@ class _HomePageState extends State<HomePage> {
               ),
               SizedBox(height: 20),
 
-              /// 商品リスト
-              GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2, // grid数
-                    crossAxisSpacing: 18, // grid間横スペース
-                    mainAxisSpacing: 20, // grid間縦スペース
-                    childAspectRatio: 0.65, // grid大きさ
-                  ),
-                  physics: NeverScrollableScrollPhysics(), // listスクロール無効
-                  shrinkWrap: true, // list余白削除
-                  itemCount: item.length,
-                  itemBuilder: (context, index) {
-                    return Column(
-                      children: [
-                        Expanded(
-                          flex: 28,
-                          child: Image.network(
-                            '${item[index]}',
-                            fit: BoxFit.cover,
+              // item test
+              item != null
+                  ? SizedBox(
+                      height: 200,
+                      child: Column(
+                        children: [
+                          Expanded(
+                            flex: 28,
+                            child: Image.network(
+                              '${item!.image}',
+                              fit: BoxFit.cover,
+                            ),
                           ),
-                        ),
-                        Expanded(
+                          Expanded(
                             flex: 13,
                             child: Column(
                               children: [
                                 Expanded(
                                   flex: 5,
                                   child: Text(
-                                    'あああああああああああああああああああああああああああああああああああああああああああああああ',
+                                    '${item!.name}',
                                     style: TextStyle(
                                       fontWeight: FontWeight.bold,
                                       fontSize: 14,
@@ -212,7 +209,7 @@ class _HomePageState extends State<HomePage> {
                                             ),
                                             SizedBox(width: 5),
                                             Text(
-                                              '0,000人以上',
+                                              '${item!.price}人以上',
                                               style: TextStyle(fontSize: 11),
                                             ),
                                           ],
@@ -229,37 +226,86 @@ class _HomePageState extends State<HomePage> {
                                   ),
                                 ),
                               ],
-                            )),
-                      ],
-                    );
-                    // return Image.network('${item[index]}');
-                  }),
-              // Column(
-              //   children: item.map((itemSelect) {
-              //     final resultEven = item.where((e) => e % 2 == 0);
-              //     return Column(
-              //       children: [
-              //         Container(
-              //           height: 250,
-              //           child: Row(
-              //             children: [
-              //               Expanded(
-              //                   child: Padding(
-              //                 padding: const EdgeInsets.all(8.0),
-              //                 child: Image.network('$itemSelect'),
-              //               )),
-              //               Expanded(
-              //                   child: Padding(
-              //                 padding: const EdgeInsets.all(8.0),
-              //                 child: Image.network('$itemSelect'),
-              //               ))
-              //             ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  : Center(child: spinKit),
+
+              /// 商品リスト
+              // GridView.builder(
+              //     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              //       crossAxisCount: 2, // grid数
+              //       crossAxisSpacing: 18, // grid間横スペース
+              //       mainAxisSpacing: 20, // grid間縦スペース
+              //       childAspectRatio: 0.65, // grid大きさ
+              //     ),
+              //     physics: NeverScrollableScrollPhysics(), // listスクロール無効
+              //     shrinkWrap: true, // list余白削除
+              //     itemCount: item.length,
+              //     itemBuilder: (context, index) {
+              //       return Column(
+              //         children: [
+              //           Expanded(
+              //             flex: 28,
+              //             child: Image.network(
+              //               '${item[index]}',
+              //               fit: BoxFit.cover,
+              //             ),
               //           ),
-              //         )
-              //       ],
-              //     );
-              //   }).toList(),
-              // ),
+              //           Expanded(
+              //               flex: 13,
+              //               child: Column(
+              //                 children: [
+              //                   Expanded(
+              //                     flex: 5,
+              //                     child: Text(
+              //                       'あああああああああああああああああああああああああああああああああああああああああああああああ',
+              //                       style: TextStyle(
+              //                         fontWeight: FontWeight.bold,
+              //                         fontSize: 14,
+              //                       ),
+              //                       overflow: TextOverflow.ellipsis,
+              //                       maxLines: 3,
+              //                     ),
+              //                   ),
+              //                   Expanded(
+              //                     flex: 2,
+              //                     child: Row(
+              //                       children: [
+              //                         Expanded(
+              //                           flex: 6,
+              //                           child: Row(
+              //                             children: [
+              //                               Icon(
+              //                                 FontAwesomeIcons.instagram,
+              //                                 color: Colors.purple,
+              //                                 size: 15,
+              //                               ),
+              //                               SizedBox(width: 5),
+              //                               Text(
+              //                                 '0,000人以上',
+              //                                 style: TextStyle(fontSize: 11),
+              //                               ),
+              //                             ],
+              //                           ),
+              //                         ),
+              //                         Expanded(
+              //                           flex: 1,
+              //                           child: Icon(
+              //                             Icons.favorite,
+              //                             color: Colors.grey,
+              //                           ),
+              //                         ),
+              //                       ],
+              //                     ),
+              //                   ),
+              //                 ],
+              //               )),
+              //         ],
+              //       );
+              //     }),
               SizedBox(height: 20)
             ],
           );
