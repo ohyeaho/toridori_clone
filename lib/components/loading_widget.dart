e5954f63ee6342cc7445077f30a3a9b194db0a02
ohyeah7275@gmail.com <ohyeah7275@gmail.com>
2021-08-25 10:37:52 +0900

    loading widget add

---
+++ lib/components/loading_widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class LoadingWidget {
+  final circleLoading = SpinKitCircle(
+    size: 100.0,
+    color: Colors.red,
+  );
+}
